4949c17a09c44edc75b71ab55dc37c05016dd2c1
vishalicious213 <vishalicious213@gmail.com>
2023-01-31 17:44:10 -0800

    rendering ingredients from array

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function renderMenuItems() {
             </div>
             <div class="menu-item-details">
                 <h3>${menuItem.name}</h3>
-                <p class="food-details">${menuItem.ingredients}</p>
+                <p class="food-details">${renderIngredients(menuItem.ingredients)}</p>
                 <p class="food-price">$${menuItem.price}</p>
             </div>
             <div class="menu-item-btns">
@@ -23,6 +23,17 @@ function renderMenuItems() {
         <hr>        
         `
     })
+}
+
+// render ingredients
+function renderIngredients(array) {
+    let ingredients = ""
+    array.forEach(function(ingredient) {
+        // console.log(ingredient)
+        // return `<span>${ingredient}</span>`
+        ingredients += `${ingredient}, `
+    })
+    return ingredients.slice(0, -2) // slice off last ", "
 }
 
 renderMenuItems()
