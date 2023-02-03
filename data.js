5e4370d332a39cb9eec99fc88fc173d93a424bac
vishalicious213 <vishalicious213@gmail.com>
2023-02-02 22:16:42 -0800

    setting up footer. added footer background image

--- data.js
+++ data.js
@@ -15,38 +15,38 @@ export const menuArray = [
         image: "/img/pizza-slice.jpg",
         id: 1
     },
-    {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
-        image: "/img/burger.jpg",
-        id: 2
-    },
-    {
-        name: "French Fries",
-        ingredients: ["potatoes", "canola oil", "salt"],
-        price: 8,
-        emoji: "🍟",
-        image: "/img/fries.jpg",
-        id: 3
-    },
-        {
-        name: "Beer",
-        ingredients: ["grain", "hops", "yeast", "water"],
-        price: 12,
-        emoji: "🍺",
-        image: "/img/beer.jpg",
-        id: 4
-    },
-    {
-        name: "Milk Shake",
-        ingredients: ["chocolate", "vanilla", "strawberry"],
-        price: 12,
-        emoji: "🥛",
-        image: "/img/shake.jpg",
-        id: 5
-    },
+    // {
+    //     name: "Hamburger",
+    //     ingredients: ["beef", "cheese", "lettuce"],
+    //     price: 12,
+    //     emoji: "🍔",
+    //     image: "/img/burger.jpg",
+    //     id: 2
+    // },
+    // {
+    //     name: "French Fries",
+    //     ingredients: ["potatoes", "canola oil", "salt"],
+    //     price: 8,
+    //     emoji: "🍟",
+    //     image: "/img/fries.jpg",
+    //     id: 3
+    // },
+    //     {
+    //     name: "Beer",
+    //     ingredients: ["grain", "hops", "yeast", "water"],
+    //     price: 12,
+    //     emoji: "🍺",
+    //     image: "/img/beer.jpg",
+    //     id: 4
+    // },
+    // {
+    //     name: "Milk Shake",
+    //     ingredients: ["chocolate", "vanilla", "strawberry"],
+    //     price: 12,
+    //     emoji: "🥛",
+    //     image: "/img/shake.jpg",
+    //     id: 5
+    // },
     {
         name: "Fountain Soda",
         ingredients: ["water", "syrup", "carbon dioxide"],
