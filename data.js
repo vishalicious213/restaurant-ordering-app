3dd929c12303d5dc4a6279c6e32f83a21c15b9b5
vishalicious213 <vishalicious213@gmail.com>
2023-02-03 00:25:09 -0800

    data.js: created mainsArray, sidesArray & drinksArray from menuArray

--- data.js
+++ data.js
@@ -104,3 +104,116 @@ export const menuArray = [
         id: 12
     }
 ]
+
+export const mainsArray = [
+    {
+        name: "Large Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 18,
+        emoji: "🍕",
+        image: "/img/pizza-large.jpg",
+        id: 0
+    },
+    {
+        name: "Small Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 14,
+        emoji: "🍕",
+        image: "/img/pizza-slice.jpg",
+        id: 1
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "mayonnaise", "lettuce"],
+        price: 10,
+        emoji: "🍔",
+        image: "/img/burger.jpg",
+        id: 2
+    },
+    {
+        name: "Cheeseburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        image: "/img/cheese-burger.jpg",
+        id: 3
+    },
+]
+
+export const sidesArray = [
+    {
+        name: "French Fries",
+        ingredients: ["potatoes", "canola oil", "salt"],
+        price: 8,
+        emoji: "🍟",
+        image: "/img/fries.jpg",
+        id: 4
+    },
+    {
+        name: "Onion Rings",
+        ingredients: ["onions", "breadcrumbs", "canola oil"],
+        price: 8,
+        emoji: "🧅",
+        image: "/img/onion-rings.jpg",
+        id: 5
+    },
+    {
+        name: "Chicken Wings",
+        ingredients: ["chicken", "breadcrumbs", "canola oil"],
+        price: 10,
+        emoji: "🍗",
+        image: "/img/wings.jpg",
+        id: 6
+    },
+    {
+        name: "Salad",
+        ingredients: ["raw greens", "croutons", "tomatoes"],
+        price: 10,
+        emoji: "🥦",
+        image: "/img/salad.jpg",
+        id: 7
+    },
+]
+
+export const drinksArray = [
+    {
+        name: "Beer",
+        ingredients: ["grain", "hops", "yeast", "water"],
+        price: 10,
+        emoji: "🍺",
+        image: "/img/beer.jpg",
+        id: 8
+    },
+    {
+        name: "Wine",
+        ingredients: ["grapes", "yeast", "sugar", "water"],
+        price: 10,
+        emoji: "🍷",
+        image: "/img/wine.jpg",
+        id: 9
+    },
+    {
+        name: "Coffee",
+        ingredients: ["coffee beans", "water", "milk foam", "sweetener"],
+        price: 8,
+        emoji: "🥛",
+        image: "/img/coffee.jpg",
+        id: 10
+    },
+    {
+        name: "Milk Shake",
+        ingredients: ["chocolate", "vanilla", "strawberry"],
+        price: 8,
+        emoji: "🥛",
+        image: "/img/shake.jpg",
+        id: 11
+    },
+    {
+        name: "Fountain Soda",
+        ingredients: ["water", "syrup", "carbon dioxide"],
+        price: 6,
+        emoji: "🥤",
+        image: "/img/soda.jpg",
+        id: 12
+    }
+]
